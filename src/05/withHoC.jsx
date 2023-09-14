a28a7455ebb673e86be572277f25830d2a1ce525
swedu <swedu.lab@kakao.com>
2023-09-14 16:57:14 +0900

    displayName 표시

--- src/05/withHoC.jsx
+++ src/05/withHoC.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 export default function withHoc(WrappedComponent){
+    const {displayName, name} = WrappedComponent;
+    const WrappedComponentName = displayName || name;
     return class WithHoC extends React.Component{
+        static displayName = `withHoc(${WrappedComponent.name})`;
         render(){
             return <WrappedComponent {...this.props} />;
         }
