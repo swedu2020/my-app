3c683c68463c0e6e1d6febe156926292f681520a
swedu <swedu.lab@kakao.com>
2023-10-05 14:30:47 +0900

    Update test01.sql

--- sql/test01.sql
+++ sql/test01.sql
@@ -75,3 +75,55 @@ insert into score(sno, kor, eng, math) values('2023010201', 111, 22, 33); -- err
 insert into score(sno, kor, eng, math) values('2023010201', -111, 22, 33); -- error
 insert into score(sno, kor, eng, math) values('2023010201', 11, 22, 33);
 select * from score;
+
+
+drop table customer;
+create table customer(
+	cid varchar(16),
+    cname varchar(50) not null,
+    cpass varchar(256) not null,
+    primary key(cid)
+);
+desc customer;
+select * from information_schema.table_constraints where table_name='customer';
+create table _order(
+	oid varchar(16),
+    cid varchar(16) not null,
+    odate date not null,
+    price decimal(15, 2) not null,
+    address varchar(256) not null,
+    ddate date,
+    money decimal(15, 2) not null,
+    dmoney decimal(15, 2) not null,
+    spoint decimal(15, 2) not null,
+    primary key(oid),
+    foreign key(cid) references customer(cid)
+);
+desc _order;
+select * from information_schema.table_constraints where table_name='_order';
+
+insert into customer(cid, cname, cpass)
+ values('C0001', '홍길동', sha1('hong'));
+insert into _order(oid, cid, odate, price, address, ddate, money, dmoney, spoint)
+ values('A0100', 'C0001', '2023-10-04', 55000, '서울',
+		'2023-10-05', 45000, 10000,  450);
+        
+INSERT INTO customer VALUES
+ ('C0002', '양바른', sha1('ybl1234')),
+ ('C0003', '유코식', sha1('uu1234')),
+ ('C0004', '김구', sha1('pass1234')),
+ ('C0005', '신사임당', sha1('pass1234'));
+ 
+INSERT INTO _order (oid, cid, odate, price, address, ddate, money, dmoney, spoint) VALUES
+ ('O1001', 'C0001', '2021-12-01', 10000, '서울시', '2021-12-01', 10000, 0, 100),
+ ('O1002', 'C0001', '2021-12-03', 4500, '부산시', '2021-12-03', 4500, 0, 45),
+ ('O1003', 'C0004', '2021-12-07', 100000, '수원시', '2021-12-07', 90000, 10000, 1000),
+ ('O1004', 'C0003', '2021-12-07', 55000, '안양시', '2021-12-07', 55000, 0, 550),
+ ('O1005', 'C0002', '2021-12-17', 85000, '대구시', '2021-12-17', 85000, 0, 850),
+ ('O1006', 'C0002', '2021-12-18', 23000, '성남시', '2021-12-18', 23000, 0, 230),
+ ('O1007', 'C0004', '2021-12-21', 5000, '대전시', '2021-12-21', 5000, 0, 50),
+ ('O1008', 'C0001', '2021-12-22', 8300, '광주시', '2021-12-22', 8300, 0, 83),
+ ('O1009', 'C0002', '2021-12-25', 45000, '고양시', '2021-12-25', 45000, 0, 450),
+ ('O1010', 'C0003', '2021-12-31', 9000, '원주시', '2021-12-31', 9000, 0, 90);
+select * from customer;
+select * from _order;
